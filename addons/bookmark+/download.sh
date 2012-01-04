e34dd36f28b9053e122d76a51fd79356474b1780
Felix Geller <fgeller@gmail.com>
2012-01-05 00:08:20 +0100

    Adding bookmark+.

---
+++ addons/bookmark+/download.sh
@@ -0,0 +1,8 @@
+curl -# -o bookmark+.el     http://www.emacswiki.org/emacs/download/bookmark%2b.el
+curl -# -o bookmark+-mac.el http://www.emacswiki.org/emacs/download/bookmark%2b-mac.el
+curl -# -o bookmark+-bmu.el http://www.emacswiki.org/emacs/download/bookmark%2b-bmu.el
+curl -# -o bookmark+-1.el   http://www.emacswiki.org/emacs/download/bookmark%2b-1.el
+curl -# -o bookmark+-key.el http://www.emacswiki.org/emacs/download/bookmark%2b-key.el
+curl -# -o bookmark+-lit.el http://www.emacswiki.org/emacs/download/bookmark%2b-lit.el
+curl -# -o bookmark+-doc.el http://www.emacswiki.org/emacs/download/bookmark%2b-doc.el
+curl -# -o bookmark+-chg.el http://www.emacswiki.org/emacs/download/bookmark%2b-chg.el
